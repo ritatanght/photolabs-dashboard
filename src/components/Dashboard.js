1368e7a555d726392729900d37895b211704dbdd
Rita Tang <doggymint@gmail.com>
2023-10-09 18:57:32 -0400

    Add function to toggle the focused state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,58 @@
 import React, { Component } from "react";
-
+import Loading from "./Loading";
+import Panel from "./Panel";
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10,
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4,
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng",
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza",
+  },
+];
 
-    return <main className={dashboardClasses} />;
+class Dashboard extends Component {
+  state = { loading: false, focused: null };
+
+  selectPanel(id) {
+    this.setState((prevState) => ({ focused: prevState.focused ? null : id }));
+  }
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
+    if (this.state.loading) {
+      return <Loading />;
+    }
+    const panels = (
+      this.state.focused
+        ? data.filter((panel) => panel.id === this.state.focused)
+        : data
+    ).map((panel) => (
+      <Panel
+        key={panel.id}
+        {...panel}
+        onSelect={() => this.selectPanel(panel.id)}
+      />
+    ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
